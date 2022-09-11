908972c056ede41ffc90634f55b28868aa95aa49
lukeAC <luke.atkinson-coyle@outlook.com>
2022-09-11 17:10:08 +0100

    state causing issues when returning the singal, signal -> 0

--- src/designs/abstract/index.js
+++ src/designs/abstract/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Sketch from "react-p5";
-import {useState, useRef} from 'react'
+import {useState, useEffect} from 'react'
 
 let signals;
 let audioContext;
 
 let setup = (p5, canvasParentRef) => {
 
-  const width = p5.windowWidth - 50
-  const height = p5.windowHeight - 50
+  const width = p5.windowWidth
+  const height = p5.windowHeight
 
   let xyz = p5.createCanvas(width, height).parent(canvasParentRef);
   let x = (p5.windowWidth - p5.width) / 2;
@@ -24,25 +24,13 @@ const frequencyBands = [
     { Eq: 'high', frequency: 2000, colour: '#85DCBA', particleX: 300, particleSize: 20, circleRadius: 100 }, 
   ];
 
-// const frequencyBands = 
-// [
-//     {bass: {frequency: 50, colour: '#C38D9E', particleX: 300, particleSize: 200, circleRadius: 10}},
-//     {lowMids : {frequency: 400, colour: '#41B3A3', particleX: 300, particleSize: 150, circleRadius: 40 }},
-//     {highids : {frequency: 800, colour: '#E8A87C', particleX: 300, particleSize: 50, circleRadius: 80}},
-//     {highs : {frequency: 2000, colour: '#85DCBA', particleX: 300, particleSize: 20, circleRadius: 100}}
-// ]
-
-//{ Eq: 'bass', frequency: 50, colour: '#C38D9E', particleX: 300, particleSize: 200, circleRadius: 10 }, 
-//     { Eq: 'lowMids', frequency: 400, colour: '#41B3A3', particleX: 300, particleSize: 150, circleRadius: 40  },
-//     { Eq: 'highMids', frequency: 800, colour: '#E8A87C', particleX: 300, particleSize: 50, circleRadius: 80 }, 
-//     { Eq: 'high', frequency: 2000, colour: '#85DCBA', particleX: 300, particleSize: 20, circleRadius: 100 }, 
-
 export default function App(p5) {
 
     const audio = new Audio();
     audioContext = new AudioContext();
 
     audio.src = '/test.mp3';
+    audio.controls = true
     const source = audioContext.createMediaElementSource(audio);
     let gainNode = audioContext.createGain();
     source.connect(gainNode);
@@ -63,61 +51,46 @@ export default function App(p5) {
           analyserNode,
           colour,
           analyserData,
-          Eq
+          Eq,
+          colour
         };
       });
 
     function draw(p5) {
         p5.background("black");
 
-        //Take signals and return a new object with the Eq tag ann signal
-
-          signals.map(({analyserNode, analyserData, Eq}, i) => {
+        //Separate audio into their different frequency band and signal strengths
+          let signalAmplitude = signals.map(({analyserNode, analyserData, Eq, colour}, i) => {
             analyserNode.getFloatTimeDomainData(analyserData);
             let signal = rootMeanSquaredSignal(analyserData);
             signal = p5.min(p5.width, p5.height)*signal
-
-            if (Eq === 'bass'){
-              p5.noStroke()
-              p5.rect (400, 500, signal)
-            }
-
-            if (Eq === 'lowMids'){
-              p5.noStroke()
-              p5.rect (500, 500, signal)
-            }
-
-            if (Eq === 'highMids'){
-              p5.noStroke()
-              p5.rect (600, 500, signal)
-            }
-
-            if (Eq === 'high'){
-              p5.noStroke()
-              p5.rect (700, 500, signal)
-            }
-
-            
-
-              // p5.noStroke()
-              // p5.fill('white')
-              // p5.circle (signal, signal, signal)
-              // p5.fill('red')
-              // p5.rectMode('CENTER')
-              // p5.rect(500, signal, signal, signal)
+            return {[Eq]: signal, 'colour': colour}
         });
+
+        const bass = signalAmplitude[0].bass
+        const lowMids = signalAmplitude[1].lowMids
+        const highMids = signalAmplitude[2].highMids
+        const highs = signalAmplitude[3].highs
+
+        p5.fill('white')
+        p5.circle(500,500,bass)
+
+        //Here is where you would draw and add different components
+        //What does using the draw function provide instead of adding an event listener to audio.currentTime ?
+
     };
 
     function handleClick(){
       audio.play();
+      console.log(audio.currentTime)
     };
     
 
   return (
-    <div style = {{position: 'absolute', top: '10px', background: 'black', paddingY: '10px', width: '80vw', height: '80vh'}}>
+    <div>
         <Sketch setup={setup} draw={draw} />
       <div style = {{position: 'absolute', top: '0px'}}>
-        <button onClick = {handleClick} style = {{width: '98vw', zIndex: 1, height: '30px', borderColor: 'black', textDecorationColor: 'white'}}>  Play  </button>
+        <button onClick = {handleClick} style = {{background: 'transparrent', width: '100vw', zIndex: 1, height: '30px', borderColor: 'black', textDecorationColor: 'white'}}>  Play  </button>
       </div>
     </div>
   );
@@ -130,3 +103,11 @@ export default function App(p5) {
     }
     return Math.sqrt(rms / data.length);
   }
+
+
+  // p5.noStroke()
+              // p5.fill('white')
+              // p5.circle (signal, signal, signal)
+              // p5.fill('red')
+              // p5.rectMode('CENTER')
+              // p5.rect(500, signal, signal, signal)
--- src/solarSystem/index.js
+++ src/solarSystem/index.js
@@ -1,121 +1,127 @@
+/* 
+- Stream Audio from URL / connect to computer audio input
+- Define starting position of the Objects using the windowWidth
+- Change colour of objects from audio
+- Change from JS into Typescript
+*/
+
 import React from 'react';
 import Sketch from "react-p5";
 import {useState, useEffect} from 'react'
-import SolarSystem from './solarSystem';
 
-/* 
-- Establish way to duplicate whole system
-- Stream Audio from URL
-- Define starting position of the Objects using the windowWidth
-- Overlay button over sketch so audio starts when sketch is clicked, re-rending causes sketch to disappear
-- Change colour of objects from audio
-- Change from JS into Typescript
+let signals;
+let audioContext;
 
-*/
+let setup = (p5, canvasParentRef) => {
 
+  const width = p5.windowWidth
+  const height = p5.windowHeight
 
-//Signals Array is empty / not defined so not mapping over anything 
-//Split into just the low frequency band and then pass to a single element as the 'bass responsive element'
-//Could just the a rectagular div that changes size () could put a border raduis on to make look cooler 
+  let xyz = p5.createCanvas(width, height).parent(canvasParentRef);
+  let x = (p5.windowWidth - p5.width) / 2;
+  let y = (p5.windowHeight - p5.height) / 2;
+  let z = (p5.windowHeight - p5.height) / 2;
+  xyz.position(x, y, z);
+}
 
 const frequencyBands = [
-  { frequency: 50, colour: '#C38D9E'}, 
-  { frequency: 200, colour: '#41B3A3'},
-  { frequency: 400, colour: '#E8A87C'}, 
-  { frequency: 800, colour: '#85DCBA'}, 
-  { frequency: 2000, colour: '#E27D60'}, 
-];
+    { Eq: 'bass', frequency: 50, colour: '#C38D9E', particleX: 300, particleSize: 200, circleRadius: 10 }, 
+    { Eq: 'lowMids', frequency: 400, colour: '#41B3A3', particleX: 300, particleSize: 150, circleRadius: 40  },
+    { Eq: 'highMids', frequency: 800, colour: '#E8A87C', particleX: 300, particleSize: 50, circleRadius: 80 }, 
+    { Eq: 'high', frequency: 2000, colour: '#85DCBA', particleX: 300, particleSize: 20, circleRadius: 100 }, 
+  ];
 
+export default function App(p5) {
 
+    const audio = new Audio();
+    audioContext = new AudioContext();
 
-function App() {
+    audio.src = '/test.mp3';
+    audio.controls = true
+    const source = audioContext.createMediaElementSource(audio);
+    let gainNode = audioContext.createGain();
+    source.connect(gainNode);
+    gainNode.connect(audioContext.destination);
 
-  const [audio, setAudio] = useState(new Audio);
-  audio.src = '/audioFile.mp3';
-  let signals = [];
+    const [bass, setBass] = useState('bass')
+    const [lowMids, setlowMids] = useState()
+    const [highMids, setHighMids] = useState()
+    const [highs, setHighs] = useState()
 
-  const audioContext = new AudioContext();
-  const source = audioContext.createMediaElementSource(audio);
-  let gainNode = audioContext.createGain();
-  source.connect(gainNode);
-  gainNode.connect(audioContext.destination);
+    
+    signals = frequencyBands.map(({ frequency, colour, Eq }) => {
+        const analyserNode = audioContext.createAnalyser();
+        analyserNode.smoothingTimeConstant = 1;
+        const analyserData = new Float32Array(analyserNode.fftSize);
+        const filterNode = audioContext.createBiquadFilter();
+        filterNode.frequency.value = frequency;
+        filterNode.Q.value = 1;
+        filterNode.type = "bandpass";
+        source.connect(filterNode);
+        filterNode.connect(analyserNode);
   
-  function handleClick(){
-    audio.play();
-  };
+        return {
+          analyserNode,
+          colour,
+          analyserData,
+          Eq,
+          colour
+        };
+      });
 
-  //Add the event listener to retrigger the singals function every thime the time updates 
-  //From the singals function an array of objects needs to be returned with each objct containing the specified values
-  //At the moment signals isn't a callable function 
-  //When converting
+      useEffect(() => {
+        if (audio){
+        audio.addEventListener('timeupdate', signalsUpdate)
+        }
+        // return () => {
+        //  if(audio){
+        //   audio.removeEventListener('timeupdate', signalsUpdate)
+        // }}
+      },[audio])
 
-  // useEffect(()=> {
-  //   console.log('effect')
-  //   audio.addEventListener('timeupdate', getSignals)
-  // }, [audio.timeupdate])
+      function signalsUpdate(){
 
+        let signalAmplitude = signals.map(({analyserNode, analyserData, Eq, colour}, i) => {
+          analyserNode.getFloatTimeDomainData(analyserData);
+   
+          let signal = rootMeanSquaredSignal(analyserData); //Take array of data and convert into average
+          signal = signal*100 //Make the singal proportional to the height / width of the screen
+          // signal = p5.min(p5.width, p5.height)*signal 
+          return {[Eq]: signal, 'colour': colour, i}
+      });
 
-  function getSignals(){
+      const bass = signalAmplitude[0].bass
+     
+      //using state to set the bass is turning the signal to 0 on the second time the singalsUpdate is called, figure out why
+     
+      };
 
-    let testVar;
+    function handleClick(){
+      audio.play();
+    };
     
-    let signals = frequencyBands.forEach(({frequency, colour}) => {
-      testVar = 'Hello testVar'
-      const analyserNode = audioContext.createAnalyser();
-      analyserNode.smoothingTimeConstant = 1;
-      const analyserData = new Float32Array(analyserNode.fftSize);
-      const filterNode = audioContext.createBiquadFilter();
-      filterNode.frequency.value = frequency;
-      filterNode.Q.value = 1;
-      filterNode.type = "bandpass";
-
-      source.connect(filterNode);
-      filterNode.connect(analyserNode);
-
-    
-      
-      // console.log(signals)
-
-      return ({testVar, signals, analyserData, analyserNode, colour})
-
-      
-    });
-
-    // console.log(analyserNode)
-
-    console.log(signals)
-
-    return testVar //analyserData //signals
-  }
-
-  // Signals is undefined, this could be because before the audio has started playing singals is 0 
-  // try the above useEffect hook
-  // try passing a const varibale through the signals function and try printing it out to se if the logic is working
-
-  function test(){
-    const string = 'Hello from Test'
-    return string
-  }
-
-
-  const data = getSignals()
-  console.log(data)
-    
-
 
   return (
     <div>
-      {/* <Sketch setup={setup} draw={draw}/> */}
-      Hello
-      <button onClick = {handleClick} style = {{width: '98vw', zIndex: 1, background: 'black', height: '30px', borderColor: 'black', textDecorationColor: 'white'}}>  Play  </button>
-
-      {/* {signals.map(({analyserNode, analyserData, colour}, i) => {
-        return(
-        <SolarSystem analyserNode = {analyserNode} colour={colour} analyserData={analyserData} canvasWidth={500} canvasHeight={500} canvasX={0} canvasY={0} />
-      // <SolarSystem audio = {audio} analyserNode = {analyserNode} colour={colour} analyserData={analyserData}  canvasWidth={500} canvasHeight={500} canvasX={500} canvasY={0}/>
-      )})} */}
+      <div style = {{position: 'absolute', top: '0px'}}>
+        <div style = {{background: 'pink', width: `${bass}px`, height: '100px'}}> {bass} </div>
+        <button onClick = {handleClick} style = {{background: 'transparrent', width: '100vw', zIndex: 1, height: '30px', borderColor: 'black', textDecorationColor: 'white'}}>  Play  </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+  function rootMeanSquaredSignal(data) {
+    let rms = 0;
+    for (let i = 0; i < data.length; i++) {
+      rms += data[i] * data[i];
+    }
+    return Math.sqrt(rms / data.length);
+  }
+
+
+  // setlowMids(signalAmplitude[1].lowMids)
+      // setHighMids(signalAmplitude[2].highMids)
+      // setHighs(signalAmplitude[3].highs)
+
+
