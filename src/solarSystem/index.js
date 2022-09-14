6fd286039c3740da6654fbcf73cd5a5afdf0e0cf
lukeAC <luke.atkinson-coyle@outlook.com>
2022-09-14 11:06:06 +0100

    figure out how to pass object to child, with colour and singnal as props

--- src/solarSystem/index.js
+++ src/solarSystem/index.js
@@ -7,28 +7,29 @@
 
 import React from 'react';
 import {useState, useEffect} from 'react'
+import Box from './box'
 
 let signals;
 let audioContext;
 
 const frequencyBands = [
-    { Eq: 'bass', frequency: 50, colour: '#C38D9E', particleX: 300, particleSize: 200, circleRadius: 10 }, 
-    { Eq: 'lowMids', frequency: 400, colour: '#41B3A3', particleX: 300, particleSize: 150, circleRadius: 40  },
-    { Eq: 'highMids', frequency: 800, colour: '#E8A87C', particleX: 300, particleSize: 50, circleRadius: 80 }, 
-    { Eq: 'high', frequency: 2000, colour: '#85DCBA', particleX: 300, particleSize: 20, circleRadius: 100 }, 
+    { Eq: 'bass', frequency: 50, colour: '#C38D9E', particleX: 300, particleSize: 200, circleRadius: 10}, 
+    { Eq: 'lowMids', frequency: 400, colour: '#41B3A3', particleX: 300, particleSize: 150, circleRadius: 40 },
+    { Eq: 'highMids', frequency: 800, colour: '#E8A87C', particleX: 300, particleSize: 50, circleRadius: 80}, 
+    { Eq: 'highs', frequency: 2000, colour: '#85DCBA', particleX: 300, particleSize: 20, circleRadius: 100}, 
   ];
 
   const audio = new Audio();
     audioContext = new AudioContext();
 
-    audio.src = '/test.mp3';
+    audio.src = '/dnb.mp3';
     audio.controls = true
     const source = audioContext.createMediaElementSource(audio);
     let gainNode = audioContext.createGain();
     source.connect(gainNode);
     gainNode.connect(audioContext.destination);
 
-  signals = frequencyBands.map(({ frequency, colour, Eq }) => {
+  signals = frequencyBands.map(({ frequency, colour, Eq, signal }) => {
     const analyserNode = audioContext.createAnalyser();
     analyserNode.smoothingTimeConstant = 1;
     const analyserData = new Float32Array(analyserNode.fftSize);
@@ -44,7 +45,6 @@ const frequencyBands = [
       colour,
       analyserData,
       Eq,
-      colour
     };
   });
 
@@ -52,10 +52,10 @@ const frequencyBands = [
 
 export default function App() {
 
-    const [bass, setBass] = useState(100)
-    // const [lowMids, setlowMids] = useState()
-    // const [highMids, setHighMids] = useState()
-    // const [highs, setHighs] = useState()
+    const [bass, setBass] = useState()
+    const [lowMids, setlowMids] = useState()
+    const [highMids, setHighMids] = useState()
+    const [highs, setHighs] = useState()
 
       useEffect(() => {
         if (audio){
@@ -68,25 +68,44 @@ export default function App() {
       },[audio])
 
       function signalsUpdate(){
-       signals.map(({analyserNode, analyserData, Eq, colour}, i) => {
+       signals.map(({analyserNode, analyserData, Eq, colour, signal}, i) => {
           analyserNode.getFloatTimeDomainData(analyserData);
-          let signal = rootMeanSquaredSignal(analyserData); //Take array of data and convert into average
-          signal = signal*1000 //Make the signal proportional to the height / width of the screen
-          setBass(signal)
-          //{[Eq]: signal, 'colour': colour}
+          signal = rootMeanSquaredSignal(analyserData); //Take array of data and convert into average
+          signal = signal*window.innerWidth*3 //Make the signal proportional to the height / width of the screen
+
+          if(Eq === 'bass'){
+            setBass(signal)
+            // setBass(signal, colour), why isn't this working ?
+          }
+          if(Eq === 'lowMids'){
+            setlowMids(signal)
+          }
+          if(Eq === 'highMids'){
+            setHighMids(signal)
+          }
+          if(Eq === 'highs'){
+            setHighs(signal)
+          }
+  
       });
+  
     }
 
     function handleClick(){
       audio.play();
     };
+
+
     
 
   return (
     <div>
-      <div style = {{position: 'absolute', top: '0px'}}>
-        <div style = {{background: 'pink', height: '100px', width: `${bass}px`}}> </div>
-        <button onClick = {handleClick} style = {{background: 'transparrent', width: '100vw', zIndex: 1, height: '30px', borderColor: 'black', textDecorationColor: 'white'}}>  Play  </button>
+    <button onClick = {handleClick} style = {{background: 'transparrent', width: '100vw', zIndex: 1, height: '30px', borderColor: 'black', textDecorationColor: 'white'}}>  Play  </button>
+      <div style = {{position: 'absolute', top: '0px', display: 'flex'}}>
+        <Box signal={bass}/>
+        <div style = {{background: 'blue', height: '100px', width: `${lowMids}px`}}> </div>
+        <div style = {{background: 'green', height: '100px', width: `${highMids}px`}}> </div>
+        <div style = {{background: 'orange', height: '100px', width: `${highs}px`}}> </div>
       </div>
     </div>
   );
@@ -99,10 +118,5 @@ export default function App() {
     }
     return Math.sqrt(rms / data.length);
   }
-
-
-  // setlowMids(signalAmplitude[1].lowMids)
-      // setHighMids(signalAmplitude[2].highMids)
-      // setHighs(signalAmplitude[3].highs)
 
 
