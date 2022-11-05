b43e2b1e50bb93216a8edf968016e6c03028fe81
lukeAC <luke.atkinson-coyle@outlook.com>
2022-11-05 17:06:24 +0000

    restructure

--- src/elements/circles.js
+++ src/elements/circles.js
@@ -4,20 +4,16 @@ import React from 'react';
 export default function Circle({output, src}) {
 
 
-  console.log(output)
+  console.log(output[0].signal)
+
   return (
-      <div>
-         {output.map(element => {
-        return(
-          <div style = {{ justifyContent:'center', alignItems: 'center', display: 'flex'
-            
-          }} >
-<div 
+    <div>
+          <div 
       style = {{
       position: 'absolute',
-      background: 'pink', 
-      width: `${50 + element.signal}vw`, 
-      height: `${50 + element.signal}vh`,  
+      // background: 'pink', 
+      width: `100vw`, 
+      height: `100vh`,  
       borderRadius: '10px', 
       // boxShadow: `inset -1px -1px 5px 5px white`, 
       transition: 'width 0.2s, height 0.2s, transform 0.2s',
@@ -25,54 +21,9 @@ export default function Circle({output, src}) {
       justifyContent: 'center', 
       // filter: 'hue-rotate(180deg)',
       alignItems: 'center' }} />  
-      <div 
-      style = {{
-      position: 'absolute',
-      background: 'blue', 
-      width: `${40 + element.signal}vw`, 
-      height: `${40 + element.signal}vh`,  
-      borderRadius: '10px', 
-      // boxShadow: `inset -1px -1px 5px 5px white`, 
-      transition: 'width 0.2s, height 0.2s, transform 0.2s',
-      display:'flex',
-      justifyContent: 'center', 
-      // filter: 'hue-rotate(180deg)',
-      alignItems: 'center' }} />  
-      <div 
-      style = {{
-      position: 'absolute',
-      background: 'red', 
-      width: `${30 + element.signal}vw`, 
-      height: `${30 + element.signal}vh`,  
-      borderRadius: '10px', 
-      // boxShadow: `inset -1px -1px 5px 5px white`, 
-      transition: 'width 0.2s, height 0.2s, transform 0.2s',
-      display:'flex',
-      justifyContent: 'center', 
-      // filter: 'hue-rotate(180deg)',
-      alignItems: 'center' }} />  
-      <div 
-      style = {{
-      position: 'absolute',
-      background: 'green', 
-      width: `${20 + element.signal}vw`, 
-      height: `${20 + element.signal}vh`,  
-      borderRadius: '10px', 
-      // boxShadow: `inset -1px -1px 5px 5px white`, 
-      transition: 'width 0.2s, height 0.2s, transform 0.2s',
-      display:'flex',
-      justifyContent: 'center', 
-      // filter: 'hue-rotate(180deg)',
-      alignItems: 'center' }} />    
-      </div>     
-      )})} 
-
-      
-
-    <img src={src} style={{height: '100%', width: '100%', borderRadius:'10px', opacity: '0.3', filter: 'contrast(50%)'}} />
       </div>
-
   );
 }
    
 
+// <img src={src} style={{height: '100%', width: '100%', borderRadius:'10px', opacity: '0.3', filter: 'contrast(50%)'}} />
