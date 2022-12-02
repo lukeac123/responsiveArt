9a6260bbed6d9656a7dc674dd1dd2120cb2bd037
Luke Atkinson-Coyle <luke.atkinson-coyle@jpmorgan.com>
2022-12-02 18:28:48 +0000

    changes

--- src/mixerTest.js
+++ src/mixerTest.js
@@ -3,27 +3,15 @@ import Sketch from "react-p5";
 import {useState, useEffect} from 'react'
 import {PlayArrow, Pause, Stop} from '@mui/icons-material';
 
+//TODO: 1. 
+//TODO: 2. open and close draw loop on play / stop
+//TODO: 3. look into smoothing constants to make less glitchy (average out the signal change)
+//TODO: 4. look into processing.js, if only using p5.js for draw loop can repace with setTimeOut
+
 let signals;
-let audioContext;
 
-function setup (p5, canvasParentRef){
-    let xyz = p5.createCanvas(500, 500).parent(canvasParentRef);
-    let x = (p5.windowWidth - p5.width) / 2;
-    let y = (p5.windowHeight - p5.height) / 2;
-    let z = (p5.windowHeight - p5.height) / 2;
-    xyz.position(x, y, z)
-  };
-
-
-const frequencyBands = [
-    { frequency: 100, colour: 'hsl(100,50%,50%)'}, 
-    { frequency: 400, colour: 'hsl(200,60%,50%)'},
-    { frequency: 800, colour: 'hsl(300,40%,50%)'}, 
-    { frequency: 1500, colour: 'hsl(400,70%,50%)'}, 
-  ];
-
-  const audio = new Audio();
-    audioContext = new AudioContext();
+const audio = new Audio();
+const audioContext = new AudioContext();
 
     if (navigator.mediaDevices) {
       navigator.mediaDevices
@@ -43,6 +31,24 @@ const frequencyBands = [
     let gainNode = audioContext.createGain();
     source.connect(gainNode);
     gainNode.connect(audioContext.destination);
+
+function setup (p5, canvasParentRef){
+    let xyz = p5.createCanvas(500, 500).parent(canvasParentRef);
+    p5.fill('black')
+    let x = (p5.windowWidth - p5.width) / 2;
+    let y = (p5.windowHeight - p5.height) / 2;
+    let z = (p5.windowHeight - p5.height) / 2;
+    xyz.position(x, y, z)
+  };
+
+
+const frequencyBands = [
+    { frequency: 100, colour: 'hsl(100,50%,50%)'}, 
+    { frequency: 400, colour: 'hsl(200,60%,50%)'},
+    { frequency: 800, colour: 'hsl(300,40%,50%)'}, 
+    { frequency: 1500, colour: 'hsl(400,70%,50%)'}, 
+  ];
+
 
   signals = frequencyBands.map(({ frequency, colour }) => {
     const analyserNode = audioContext.createAnalyser();
@@ -76,7 +82,7 @@ export default function Mixer() {
     setPlaying(false);
   }}
 
-      function draw(){
+      function draw(p5){
        let data = signals.map(({analyserNode, analyserData, colour}, i) => {
           analyserNode.getFloatTimeDomainData(analyserData);
           let signal = rootMeanSquaredSignal(analyserData); 
@@ -86,7 +92,19 @@ export default function Mixer() {
           setAnalyserData(analyserData) 
           return {signal, colour, i, analyserData}
       });
-      setOutput(data)   
+      setOutput(data)  
+      p5.fill('blue') 
+      p5.circle(300,300,output[0]?.signal*100)
+
+      p5.fill('red') 
+      p5.circle(300,300,output[1]?.signal*100)
+
+      p5.fill('green') 
+      p5.circle(300,300,output[2]?.signal*100)
+
+      p5.fill('pink') 
+      p5.circle(300,300,output[3]?.signal*100)
+
     }
 
 
@@ -104,11 +122,7 @@ export default function Mixer() {
 
       <span className='buttonBar' style = {{height: '30px'}} >
       <button className='button' classes={{fill: 'white', colour: 'white'}} onClick = {handlePlay} >
-        {playing? <Pause/>:<PlayArrow/>}
-      </button>
-      <button className='button'  onClick = {''} > 
-      {/* onClick close the audio context */}
-        <Stop/>
+        {playing? <Stop/>:<PlayArrow/>}
       </button>
       </span>
 
